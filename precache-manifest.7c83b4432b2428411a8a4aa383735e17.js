82d50a295606e05192f25e925526304da3605fea
ERDAG KUCUKDEMIRCI <erdag@ERDAGs-MBP.attlocal.net>
2020-06-12 18:48:22 -0700

    Updates

---
+++ precache-manifest.7c83b4432b2428411a8a4aa383735e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcec6dc3f26e5b6d5491b1ce04ed6e4",
+    "url": "/poke/index.html"
+  },
+  {
+    "revision": "081be46d644d1fa20c38",
+    "url": "/poke/static/css/main.a349f5fd.chunk.css"
+  },
+  {
+    "revision": "d503304785c2a0e88b1a",
+    "url": "/poke/static/js/2.38bdd23a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/poke/static/js/2.38bdd23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081be46d644d1fa20c38",
+    "url": "/poke/static/js/main.e4947695.chunk.js"
+  },
+  {
+    "revision": "51d41dd463607af1f561",
+    "url": "/poke/static/js/runtime-main.98b073c1.js"
+  }
+]);
